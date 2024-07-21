19179b5ddcc934f57461ca92c7ea31b1ce4b8844
Idan Shalom <idan.shalom@intl.att.com>
2024-07-21 12:48:33 +0300

    update di

--- src/main/java/com/att/tdp/todo_app/services/TodoService.java
+++ src/main/java/com/att/tdp/todo_app/services/TodoService.java
@@ -1,64 +1,10 @@
 package com.att.tdp.todo_app.services;
 
-import com.att.tdp.todo_app.dal.TodoRepository;
-import com.att.tdp.todo_app.dto.TodoEntity;
-import com.att.tdp.todo_app.dto.TodoRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TodoService {
-
-    @Autowired
-    private TodoRepository todoRepository;
-
-    public List<TodoEntity> getTodos() {
-        return todoRepository.findAll();
-    }
-
-    public Optional<TodoEntity> getTodo(Long id) {
-        return todoRepository.findById(id);
-    }
-
-    public TodoEntity createTodo(TodoRequest todoRequest) {
-        TodoEntity todo = new TodoEntity();
-        todo.setTitle(todoRequest.getTitle());
-        todo.setDescription(todoRequest.getDescription());
-        return todoRepository.save(todo);
-    }
-
-    public TodoEntity updateTodo(Long id, TodoRequest todoRequest) {
-        Optional<TodoEntity> todoEntity = todoRepository.findById(id);
-        if (todoEntity.isEmpty()) {
-            return null;
-        }
-
-        TodoEntity updatedTodo = todoEntity.get();
-        if (todoRequest.getTitle() != null) {
-            updatedTodo.setTitle(todoRequest.getTitle());
-        }
-
-        if (todoRequest.getDescription() != null) {
-            updatedTodo.setDescription(todoRequest.getDescription());
-        }
-
-        if (todoRequest.getIsCompleted() != null) {
-            updatedTodo.setCompleted(todoRequest.getIsCompleted());
-        }
-
-        todoRepository.save(updatedTodo);
-        return updatedTodo;
-    }
-
-    public Long deleteTodo(Long id) {
-        if (todoRepository.findById(id).isPresent()) {
-            todoRepository.deleteById(id);
-            return id;
-        }
-
-        return null;
+    public String sayHello(String name) {
+        return "Hello " + name;
     }
 }

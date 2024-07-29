3ec2009779f7d1e5c6a80b6541a7f227dc272fb0
Idan Shalom <idan.shalom@intl.att.com>
2024-07-29 09:41:04 +0300

    update stuff

--- src/main/java/com/att/tdp/todo_app/services/TodoService.java
+++ src/main/java/com/att/tdp/todo_app/services/TodoService.java
@@ -34,6 +34,7 @@ public class TodoService {
         TodoEntity todo = new TodoEntity();
         todo.setTitle(todoRequest.getTitle());
         todo.setDescription(todoRequest.getDescription());
+        todo.setCompleted(false);
         return todoRepository.save(todo);
     }
 

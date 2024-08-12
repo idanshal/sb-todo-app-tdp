a29abc5fa11d449571d12118a03a3271e68865ba
Idan Shalom <idan.shalom@intl.att.com>
2024-08-13 00:20:30 +0300

    add stuff

---
+++ src/test/java/com/att/tdp/todo_app/TodoServiceTest.java
@@ -0,0 +1,43 @@
+package com.att.tdp.todo_app;
+
+import com.att.tdp.todo_app.dal.TodoRepository;
+import com.att.tdp.todo_app.dto.CreateTodoRequest;
+import com.att.tdp.todo_app.dto.TodoEntity;
+import com.att.tdp.todo_app.services.TodoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TodoServiceTest {
+
+    @Mock
+    private TodoRepository todoRepository;
+
+    @InjectMocks
+    private TodoService todoService;
+
+    @Test
+    void testCreateTodo() {
+        // arrange
+        CreateTodoRequest request = new CreateTodoRequest();
+        request.setTitle("Test");
+        request.setDescription("Test Description");
+        // act
+        todoService.createTodo(request);
+        // assert
+        ArgumentCaptor<TodoEntity> captor = ArgumentCaptor.forClass(TodoEntity.class);
+        verify(todoRepository).save(captor.capture());
+        TodoEntity capturedTodo = captor.getValue();
+        assertThat(capturedTodo.getTitle()).isEqualTo("Test");
+        assertThat(capturedTodo.getDescription()).isEqualTo("Test Description");
+        assertThat(capturedTodo.isCompleted()).isFalse();
+    }
+}
